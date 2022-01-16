26fa0cbc0664341cac47ef05fc6ec79a8911eefb
Yevhen Malyshko <memx86@gmail.com>
2022-01-16 16:06:20 +0200

    code prettify

--- src/index.js
+++ src/index.js
@@ -24,12 +24,13 @@ function onInput(e) {
 }
 function getCountryByName(country) {
   const url = `https://restcountries.com/v3.1/name/${country}?fields=name,capital,population,flags,languages`;
-  return fetch(url).then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error(response.statusText);
-  });
+  return fetch(url).then(handleResponse);
+}
+function handleResponse(response) {
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error(response.statusText);
 }
 function onSuccess(countries) {
   if (countries.length === 1) {
